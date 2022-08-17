1d6ed42023358f202b8b419d5bf6caa6bd61ac19
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-08-16 20:21:31 -0500

    courseReact

---
+++ src/components/Eventos.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+export default class Eventos extends Component{
+    constructor(props){
+        super(props);
+            this.state = {
+                contador: 0,
+            }
+            this.sumar = this.sumar.bind(this)
+            this.restar = this.restar.bind(this)
+    }
+    sumar(e) {
+        console.log("Sumando")
+        this.setState({
+            contador: this.state.contador + 1,
+        })
+    }
+    restar(e) {
+        console.log("Restando")
+        this.setState({
+            contador: this.state.contador - 1,
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h2>Eventos en componentes de Clase</h2>
+                <nav>
+                    <button onClick={this.restar}>-</button>
+                    <button onClick={this.sumar}>+</button>
+                </nav>
+                <h3>{this.state.contador}</h3>
+            </div>
+        )
+    }
+}
