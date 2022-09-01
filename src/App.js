a773dbb266ce5b6c806d88e5ac4719267b2109ff
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-08-31 21:37:50 -0500

    useEffect

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ComunicacionComponentes from './components/ComunicacionComponentes'
 import CicloVida from './components/CicloVida';
 import AjaxApis from './components/AjaxApis';
 import ContadorHooks from './components/ContadorHooks';
+import ScrollHooks from './components/ScrollHooks';
 
 function App() {
   return (
@@ -63,6 +64,8 @@ function App() {
           <AjaxApis/>
           <hr/>
           <ContadorHooks titulo="Seguidores"/>
+          <hr/>
+          <ScrollHooks/>
         </section>
       </header>
     </div>
---
+++ src/components/ScrollHooks.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+export default function ScrollHooks(){
+    const [scrollY, setScrollY] = useState(0);
+    useEffect(() => {
+        console.log("Moviendo el scroll");
+
+        const detectarScroll = () => setScrollY(window.pageYOffset);
+
+        window.addEventListener("scroll", detectarScroll);
+
+        return () => {
+            window.removeEventListener("scroll", detectarScroll);
+            console.log("Fase de desmontaje");
+        }
+    }, [scrollY]);
+
+    useEffect(() => {
+        console.log("Fase de montaje");
+    }, [])
+
+    useEffect(() => {
+        console.log("Fase de actualización");
+    })
+
+    useEffect(() => {
+        return () => {
+            console.log("Fase de desmontaje");
+        }
+    })
+    return(
+        <>
+            <h2>Hooks - useEffect y el ciclo de vida</h2>
+            <p>Scroll Y del navegador {scrollY}px</p>
+        </>
+    )
+}
