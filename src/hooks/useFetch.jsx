195eec0e2b9048ebab7258971d4384081b2e6ce6
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-09-13 22:16:30 -0500

    someChanges

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,5 +1,28 @@
 import { useState, useEffect } from 'react';
 
 export const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        const getData = async (url) => {
+            let res = await fetch(url),
+            json = await res.json();
 
+
+            json.results.forEach(async (el) => {
+                let res = await fetch(el.url),
+                    json = await res.json();
+                    
+                    let pokemon = {
+                        id: json.id,
+                        name: json.name,
+                        avatar: json.sprites.front_default
+                    };
+                    setPokemons((pokemons) => [...pokemons, pokemon]);
+            })
+        }
+        getData(url);
+    }, [url]);
+    return {data, isPending, error}
 }
