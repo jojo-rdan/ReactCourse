a6f86a48eba46b69c7c42acafd54904145aac5d5
Jordan Gonzalez <jordan.olaves91@gmail.com>
2022-09-14 22:18:53 -0500

    errHandler

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -6,21 +6,23 @@ export const useFetch = (url) => {
     const [error, setError] = useState(null);
     useEffect(() => {
         const getData = async (url) => {
-            let res = await fetch(url),
-            json = await res.json();
-
-
-            json.results.forEach(async (el) => {
-                let res = await fetch(el.url),
-                    json = await res.json();
-                    
-                    let pokemon = {
-                        id: json.id,
-                        name: json.name,
-                        avatar: json.sprites.front_default
-                    };
-                    setPokemons((pokemons) => [...pokemons, pokemon]);
-            })
+            try {
+                let res = await fetch(url);
+                if(!res.ok){
+                    throw {
+                        err:true, 
+                        status: res.status, 
+                        statusText: !res.statusText ? "Ocurrió un error" : res.statusText
+                    }
+                }
+                let data = await res.json();
+                setIsPending(false);
+                setData(data);
+                setError({err: false});
+            } catch (err) {
+                setIsPending(true);
+                setError(err);
+            }
         }
         getData(url);
     }, [url]);
